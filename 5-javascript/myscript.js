3ddeb4e34ca033daa1024443782b1957a4d327cc
Kancherla Teja <youremail@example.com>
2026-06-06 15:41:44 +0530

    langa varun

--- 5-javascript/myscript.js
+++
@@ -1,7 +0,0 @@
-window.document.write("Welcome to javaScript Programming ..<br>")
-document.write("Hello JavaScript<br> ")
-let a1=10;
-let a2=20;
-let sum=a1+a2;
-document.write("<br> Sum of two values :",Sum);
-console.log("<br>Sum of two values :" ,sum);
